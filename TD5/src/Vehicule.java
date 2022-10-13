da931a7b73a0af4c0430dd55b7213f07d0e7c2bd
victor <victor.lath9@etu.univ-lorraine.fr>
2022-10-13 11:29:24 +0200

    ajout classe vehicules

--- TD5/src/Vehicule.java
+++ TD5/src/Vehicule.java
@@ -1,2 +1,14 @@
 public abstract class Vehicule {
+    private String type;
+    private double vitesseMax;
+    private double vitesse;
+
+    public Vehicule(){
+    }
+
+    public double getVitesse(){
+        return vitesse;
+    }
+    public
+
 }
